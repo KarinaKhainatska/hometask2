d509c9c7edfbd625cad332feff34ef8db6285b2b
KarinaKhainatska <hajnackakarina@gmail.com>
2025-10-06 22:53:35 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,7 @@ elements.forEach(el => {
     el.addEventListener("mouseleave", () => {
         el.style.backgroundColor = prev;
     });
-});*/
+});
 setTimeout(() => {
 let imagesUrl = [
     "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-card-40-17pro-202509_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=WVVFRzUzVk1oblJhbW9PbGNSU25ja3doNjVzb1FWSTVwZWJJYThYTHlrNzQzbUlIR1RvazhDRHNOQlYvM3g2dFIwdkZSSnBZYjhOaHBpM2lkYTFBUEZHTmVoMWFVZloyU3lqdmZCOUFEeDF6K2N6UFd4K21VWHNnbWZBQ3hSanQ",
@@ -41,3 +41,28 @@ imgarrayURL.appendChild(img);
 });
 //imgarrayURL.appendChild(images);
 },5000);
+*/
+let oldString=new Set();
+document.getElementById("check").onclick=function(){
+    let input=document.getElementById("inputText").value.trim();
+    if (input.length===0){
+        return -1;
+    }
+let inputSutring= new Set(input
+    .toLowerCase()
+    .replace(/[.,!?]/g, "")
+    .split(/\s+/)
+);
+if  (oldString.size>0){
+    let repeatWords = [...inputSutring].filter(word => oldString.has(word));
+    if (repeatWords.length>0){
+        document.getElementById("result").innerText="Repeated words: "+repeatWords.join(", ");
+    } else {
+        document.getElementById("result").innerText="No repeated words";
+    }
+} else {
+    document.getElementById("result").innerText="Enter one more time";
+}
+oldString=inputSutring;
+document.getElementById("inputText").value="";
+};
